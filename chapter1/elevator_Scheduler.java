8cf910cdc0a0883c4191603d2063a61c84e8827b
wenqiang <wmiao1122@gmail.com>
2012-09-26 15:55:57 -0400

    u

--- chapter1/elevator_Scheduler.java
+++ chapter1/elevator_Scheduler.java
@@ -5,7 +5,7 @@ public class elevator_Scheduler{
     private int minSumFloor;
     //private int[] nPerson;
 
-    public void schedule(int[] nPerson,int N){
+    public void schedule1(int[] nPerson,int N){
         for(int i = 1; i <= N;++i){
             int sum = 0;
             for(int j = 1; j < i;++j){
@@ -16,9 +16,50 @@ public class elevator_Scheduler{
             }
             if(nTargetFloor == -1 || sum < minSumFloor){
                 minSumFloor = sum;
-                nTargerFloor = i;
+                nTargetFloor = i;
             }
         }
+    }
+
+    public void schedule2(int[] nPerson,int N){
+        int sum = 0;
+        int lSum = 0;
+        int hSum = 0;
+        int lPPL = nPerson[1];  //not including the current ppl
+        int hPPL = 0;           //current ppl to the end
+        nTargetFloor = 1;
+        for(int i = 2; i <= N;++i){
+            minSumFloor += nPerson[i]*(i-1);
+            hPPL += nPerson[i];
+        }
+        lSum = 0;
+        hSum = minSumFloor;
+        
+        for(int i = 2; i <= N;++i){
+            lSum += lPPL;
+            lPPL += nPerson[i];
+            hSum -= hPPL;
+            hPPL -= nPerson[i];
+            sum = lSum+hSum;
+
+            if(sum < minSumFloor){
+                minSumFloor = sum;
+                nTargetFloor = i;
+            }
+        }
+
+    }
+
+    public static void main(String[] args){
+        elevator_Scheduler test = new elevator_Scheduler();
+        test.nTargetFloor = -1;
+        test.minSumFloor = 0;
+
+        int[] nPerson = {-1,1,2,3,4,5,6,7,8,9};
+        test.schedule1(nPerson,9);
+        System.out.println(test.nTargetFloor);
+        System.out.println(test.minSumFloor);
+
     }
 
 }
